d4e1490c1f4bad7b580bf00b5dc6f5594813bc9c
RAJ <mdrajahmad686@gmail.com>
2024-10-06 20:21:16 +0600

    app bar complete

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
 
+          //Use Background Color on App Bar
+          backgroundColor: Colors.indigoAccent,
+
+          //App bar Title
+          //App bar Title
+          title: Text("Home"),
+          centerTitle: true,
+
+          //Use icon Code
+          //Use icon Code
+          actions: <Widget>[
+            IconButton(onPressed: null, icon: Icon(Icons.notification_add) )
+          ],
+
+
+          //Use left Side Icons Code
+          //Use left Side Icons Code
+          leading: Icon(Icons.menu),
+
+        ),
       ),
     );
   }
 }
-
-
