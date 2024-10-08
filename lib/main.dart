de05fea5bb0acf3fc818292b1bb8457ea002c433
RAJ <mdrajahmad686@gmail.com>
2024-10-08 13:22:35 +0600

    Container Code

--- lib/main.dart
+++ lib/main.dart
@@ -9,41 +9,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
 
-        //##################Text Widget Start##################
-         body: Center(
-           child: Text("HELLO WORLD FLUTTER", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),),
-         ),
-        //##################Text Widget End##################
-
-        //##################APP Bar code start##################
-        //##################APP Bar code start##################
-        appBar: AppBar(
-
-          //Use Background Color on App Bar
-          backgroundColor: Colors.indigoAccent,
-
-          //App bar Title
-          //App bar Title
-          title: Text("Home"),
-          centerTitle: true,
-
-          //Use icon Code
-          //Use icon Code
-          actions: <Widget>[
-            IconButton(onPressed: null, icon: Icon(Icons.notification_add) )
-          ],
-
-
-          //Use left Side Icons Code
-          //Use left Side Icons Code
-          leading: Icon(Icons.menu),
-
-        ),
-        //##################APP Bar code End##################
-
+      home: SafeArea(
+          child: Scaffold(
+            body: Container(
+               height: 200,
+              width: 300,
+             color: Colors.amber,
+            ),
+          )
       ),
+
     );
   }
 }
